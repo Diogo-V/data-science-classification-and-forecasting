c7dea2b8af7cb0592569f2739c485a8d3be5378b
Diogo-V <diogo.venancio00@gmail.com>
2023-01-03 11:41:31 +0000

    feat: health scaling?

--- health-forecasting/src/main.py
+++ health-forecasting/src/main.py
@@ -4,18 +4,23 @@ from sklearn.model_selection import train_test_split
 
 from profiling.profiling import Profiler
 from preparation.mvi_imputation import MVImputation
+from preparation.scaling import Scaling
 
 INPUT_FILE_PATH = 'health-forecasting/resources/data/glucose.csv'
 
 if __name__ == "__main__":
 
     # ----------------------------- 1º Phase -> Data profiling ----------------------------- #
-    data = read_csv(INPUT_FILE_PATH, sep=',', decimal='.', parse_dates=True, infer_datetime_format=True)
+    data = read_csv(INPUT_FILE_PATH, index_col='Date', sep=',', decimal='.', parse_dates=True, infer_datetime_format=True)
 
     #  profiler = Profiler(data)
     #  profiler.explore_count_data_types()
 
     # ----------------------------- 2º Phase -> Data preparation ----------------------------- #
-    mvi = MVImputation(data)
-    data = mvi.approach_1()
-    #data = mvi.approach_2()
+    # mvi = MVImputation(data)
+    # data = mvi.approach_1()
+    # data = mvi.approach_2()
+
+    ## Does it even make sense to apply scaling?
+    # scaling = Scaling(data)
+    # data = scaling.explore_scaling()
