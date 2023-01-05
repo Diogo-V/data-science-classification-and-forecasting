a29eadb3270ca5ff7312ca8b6f696f829a67bc64
Carlos Sousa <carl0smvschacinador@gmail.com>
2023-01-05 13:11:20 +0000

    Aggregation and differentation for health dataset

--- health-forecasting/src/main.py
+++ health-forecasting/src/main.py
@@ -6,6 +6,7 @@ from profiling.profiling import Profiler
 from preparation.mvi_imputation import MVImputation
 from preparation.scaling import Scaling
 
+from transformation.aggregation import Aggregation
 from transformation.differentiation import Differentiation
 
 INPUT_FILE_PATH = 'health-forecasting/resources/data/glucose.csv'
@@ -19,11 +20,16 @@ if __name__ == "__main__":
     #  profiler.explore_count_data_types()
 
     # ----------------------------- 2º Phase -> Data preparation ----------------------------- #
-    mvi = MVImputation(data)
-    mvi.explore_mv_imputation()
-    data = mvi.compute_mv_imputation("approach_2")
+    # mvi = MVImputation(data)
+    # mvi.explore_mv_imputation()
+    # data = mvi.compute_mv_imputation("approach_2")
 
     # No scaling applied due to few columns in dataset
-    
+
+    aggregation = Aggregation(data)
+    aggregation.explore_aggregation()
+    data = aggregation.compute_aggregate_daily(data)
+
     differentiation = Differentiation(data)
+    differentiation.explore_differentiation()
     data = differentiation.compute_differentiation()
